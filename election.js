a17997f4d2de696e523e16ec6fd0d07d6fa6ad94
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-17 09:38:22 -0500

    Add event listener to post vote for a candidate

--- election.js
+++ election.js
@@ -9,8 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
   }).done(function(data) {
     data.candidates.forEach(function(candidate) {
       var li = document.createElement('li');
-      li.innerText = candidate.name + ' has ' + candidate.votes + ' votes.'
-      ul.append(li)
+      li.innerText = candidate.name + ' has ' + candidate.votes + ' votes.';
+      var form = document.createElement('form');
+      form.addEventListener('submit', function() {
+        $.ajax({
+          url: 'https://bb-election-api.herokuapp.com/vote' + candidate.name,
+          method: 'POST'
+        })
+      })
+      li.append(form);
+      ul.append(li);
     })
   })
 
